979f1619b033073464af8c75ea107b96884e27de
Farouk19990 <>
2025-04-22 22:45:14 +0100

    Vapi API Integration

---
+++ frontend/src/ai.js
@@ -0,0 +1,8 @@
+import Vapi from "@vapi-ai/web";
+
+const vapi = new Vapi(import.meta.env.VITE_VAPI_API_KEY)
+const assistantId = import.meta.env.VITE_ASSISTANT_ID
+
+export const startAssistant = async(full_name,age,has_done_therapy_before,therapy_reasons,therapist_gender_preference,session_format,availability,payment_method)=>{
+    return vapi.start(assistantId)
+}
