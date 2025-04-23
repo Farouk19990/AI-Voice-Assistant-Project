1707f128a1a54ac0ce37455b11b90eaf614c1a37
Farouk19990 <>
2025-04-23 01:06:10 +0100

    wip: frontend form setup and animation trigger

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,40 @@
-import { useState } from 'react'
-
-
+import { useState,useEffect } from 'react'
+import {vapi,startAssistant,stopAssistant} from "./ai"
+import './app.css'
 function App() {
+ /* useEffect(()=>{
+    startAssistant()
+  },[])*/
   const [count, setCount] = useState()
 
   return (
-    <>
-    </>
-  )
+	<div className="main">  	
+	<input type="checkbox" id="chk" aria-hidden="true" />
+  
+	<div className="signup">
+	  <form>
+		<label className="sophie" aria-hidden="true">
+			<div className="sophie-text">
+			<p className="sophie-title">Talk to Sophie, your virtual assistant 👩‍⚕️</p>
+		<p className="sophie-subtext">Need help booking your therapy session? Sophie will ask a few quick questions to help you.</p></div></label>
+		<input type="email" name="email" placeholder="Email" required />
+		<input type="number" name="phone" placeholder="Phone Number" required />
+		<button>Sign up</button>
+	  </form>
+	</div>
+  
+	<div className="login">
+  <div className="call-ui">
+    <div className="pulse-circle" ></div>
+    <p className="call-text">Calling Sophie... 👩‍⚕️</p>
+    <p className="call-subtext">Hang tight, she’s preparing your session 🧠💬</p>
+  </div>
+</div>
+
+  </div>
+  
+)
 }
 
 export default App
+
--- frontend/src/ai.js
+++ frontend/src/ai.js
@@ -1,8 +1,12 @@
 import Vapi from "@vapi-ai/web";
 
-const vapi = new Vapi(import.meta.env.VITE_VAPI_API_KEY)
+export const vapi = new Vapi(import.meta.env.VITE_VAPI_API_KEY)
 const assistantId = import.meta.env.VITE_ASSISTANT_ID
 
 export const startAssistant = async(full_name,age,has_done_therapy_before,therapy_reasons,therapist_gender_preference,session_format,availability,payment_method)=>{
     return vapi.start(assistantId)
+}
+
+export const stopAssistant= () =>{
+    vapi.stop()
 }
